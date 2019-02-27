4085226888c9611030bc462c294f2f84c6fbe7dd
Senay Yakut <senaykt@gmail.com>
2019-02-26 19:33:57 -0800

    flask is listening

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask 
+from flask import Flask, render_template 
 
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    return "<html><body><h1>Hi!</h1></body></html>"
+    return render_template("homepage.html")
+    #return "<html><body><h1>Hi!</h1></body></html>"
+
+@app.route("/another")
+def some_other_name():
+    return "<html><body><h1>Yo!</h1></body></html>"    
